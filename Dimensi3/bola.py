c293f7a28bd5a73b034577dd8e0f99325b10a93e
Haqqul Farjri H <82302378+haqqulfh@users.noreply.github.com>
2021-04-11 22:50:49 +0800

    Update bola.py

--- Dimensi3/bola.py
+++ Dimensi3/bola.py
@@ -13,7 +13,7 @@ def getHasil(r):
     hasil = "Volume = " + str(getVolume(r)) + "\n" + "Luas Permukaan = " + str(getLuasPermukaan(r))
     return hasil
 
-def checkError(nama, value) :
+def checkError(inputUsers, nama, value) :
     if nama == "jari-jari"  :
         if value <= 0 :
             return nama + " tidak boleh kurang dari atau sama dengan 0"
