f59d421ec78694ace3c314c51ce1a1da4fd926b9
Haqqul Farjri H <82302378+haqqulfh@users.noreply.github.com>
2021-04-11 20:26:57 +0800

    Update bola.py

--- Dimensi3/bola.py
+++ Dimensi3/bola.py
@@ -2,11 +2,11 @@ def getAttributes():
     return ["jari-jari"]
 
 def getVolume(r):
-    volume = (4/3) * 3.14 * r ** 3
+    volume = (22/7) * r ** 3 * 4 / 3
     return volume
 
 def getLuasPermukaan(r):
-    luas_permukaan = 4 * 3.14 * r ** 2
+    luas_permukaan = 4 * (22/7) * r ** 2
     return luas_permukaan
 
 def getHasil(r):
