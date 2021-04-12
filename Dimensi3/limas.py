b149eadbb5a83e2640846f5c743419103fbf38c9
vincentiadk <vincentiadksitanggang@gmail.com>
2021-04-12 16:55:54 +0700

    Update limas.py

--- Dimensi3/limas.py
+++ Dimensi3/limas.py
@@ -31,24 +31,17 @@ def checkError(inputUsers, nama, value) :
             return nama + " tidak boleh kurang dari atau sama dengan 0."
         else : 
             return True
-    # elif nama == "panjang selimut" :
-    #     if value <= 0 :
-    #         return nama + " tidak boleh kurang dari atau sama dengan 0."
-    #     else : 
-    #         return True
     elif nama == "panjang selimut" :
-        if value < inputUsers[-1] :
+        if value <= 0 :
+            return nama + " tidak boleh kurang dari atau sama dengan 0."
+        elif value < inputUsers[-1] :
             return nama + " tidak boleh kurang dari panjang sisi : {} cm.".format(inputUsers[-1])
         else : 
             return True
 
 def getHasil(n, sisi, panjang_selimut):
-    try:
         l_permukaan = luas_permukaan(n, sisi, panjang_selimut)
-        vol = volume(n, sisi, panjang_selimut)
-    except:
-        return 'Error!! Nilai panjang selimut kurang besar.'
-    else:   
+        vol = volume(n, sisi, panjang_selimut)   
         n = int(n)
         out = "\nLuas permukaan prisma segi-{0} beraturan = {1:.2f} cm\u00b2\nVolume prisma segi-{0} beraturan = {2:.2f} cm\u00b3"
         
